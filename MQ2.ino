c613234c24377a75b12a23e822784dcd6f2eb57b
omkarnangnure26 <40233799+omkarnangnure@users.noreply.github.com>
2019-04-18 22:59:33 +0530

    Add files via upload

---
+++ MQ2.ino
@@ -0,0 +1,51 @@
+ //Program to get values from MQ2 gas sensor and give interpretation according to proposed title
+int yellowLed=8;           //Blue Led is connected to digital pin no.8
+int redLed = 10;          //Red Led is connected to  digital pin no.10
+int greenLed = 12;       //Green Led is connected to digital pin no. 12
+int smokeA0 = A5;        //Analog pin is connected analog pin no.A5
+
+int sensorThres1 = 440;   //First threshold value
+int sensorThres2 = 800;    //Second threshold value
+
+void setup() {
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+  Serial.println("Getting ready ..  checking the sensor....");
+  Serial.println("Calculating values....");
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+   
+  Serial.print("Value is: ");
+  Serial.println(analogSensor);
+  
+  // Checks if it has reached the threshold value
+   if (analogSensor < sensorThres1 )
+  {
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    digitalWrite(redLed, LOW);
+
+    delay(100);
+  }
+  else if (analogSensor > sensorThres1 && analogSensor < sensorThres2)
+  {
+    digitalWrite(yellowLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(redLed, LOW);
+
+    delay(100);
+  }
+  else if(analogSensor > sensorThres2)
+  {
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(yellowLed, LOW);
+   
+    delay(100);
+  }
+}
---
+++ finalservo.ino
@@ -0,0 +1,29 @@
+#include<Servo.h>   
+
+Servo sg90; 
+int servo_pin = 11;
+int angle=0;
+void setup() 
+{
+  // put your setup code here, to run once
+  sg90.attach ( servo_pin );
+  pinMode(6,INPUT);
+sg90.write(180);
+Serial.begin(9600); 
+}
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+ 
+  for(digitalRead(6)==HIGH; ; )
+  {
+    Serial.print("1\n");
+    sg90.write(0);
+    delay(3000);
+    sg90.write(180);
+    delay(1000);
+    break;   
+     }
+  }
+
+
